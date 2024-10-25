7b310aa424bc748ffeec2d9638fba1f085059a30
mariu <marius.cloete@gmail.com>
2024-10-25 22:25:16 +0200

    Refactor copyCode function to simplify clipboard text copying

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,5 @@ function copyCode(elementId) {
     const codeElement = document.getElementById(elementId);
     const text = codeElement.innerText;
   
-    navigator.clipboard.writeText(text).then(() => {
-      alert('Code copied to clipboard!');
-    }).catch(err => {
-      console.error('Failed to copy text: ', err);
-    });
+    navigator.clipboard.writeText(text);
   }
